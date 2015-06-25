75d4642770b56f908f11d577548b8bc7ebe5c217
Eric Su <eric.cc.su@gmail.com>
2015-06-25 10:53:24 -0400

    :wrench: Backend, spacing changes

--- locker.py
+++ locker.py
@@ -1,7 +1,7 @@
 try:
-	from cryptography.fernet import Fernet
+    from cryptography.fernet import Fernet
 except:
-	raise Exception("The system is not properly initialized")
+    raise Exception("The system is not properly initialized")
 
 import json
 import lockops
@@ -15,145 +15,147 @@ MTB = {}
 UPRF = ""
 
 def gen_screen(prompt, input_text="INPUT: ", back=False, menu=None):
-	os.system('cls')
-	print("\t*************************************************")
-	if menu is None:
-		print("\t***                  Locker                   ***")
-	else:
-		print(menu)
-	print("\t***                                           ***")
-	print("\t***                                           ***")
-	print(prompt)
-	print("\t***                                           ***")
-	print("\t***                                           ***")
-	print("\t***                                           ***")
-	print("\t***                                           ***")
-	print("\t***                                           ***")
-	if back:
-		print("\t***           :back  - previous menu          ***")
-	else:
-		print("\t***         :q     - close application        ***")
-	print("\t*************************************************")
-	return( input(input_text) )
+    os.system('cls')
+    print("\t*************************************************")
+    if menu is None:
+        print("\t***                  Locker                   ***")
+    else:
+        print(menu)
+    print("\t***                                           ***")
+    print("\t***                                           ***")
+    print(prompt)
+    print("\t***                                           ***")
+    print("\t***                                           ***")
+    print("\t***                                           ***")
+    print("\t***                                           ***")
+    print("\t***                                           ***")
+    if back:
+        print("\t***           :q   - previous menu            ***")
+    else:
+        print("\t***         :q     - close application        ***")
+    print("\t*************************************************")
+    return( input(input_text) )
 
 def screen_decrypt():
-	prompt = "\t*** INPUT ENTRY LABEL                         ***"
-	menu = "\t***              Locker - Decrypt             ***"
-	ks = gen_screen(prompt ,'LABEL: ', True).strip().lower()
-	if ks == ":back":
-		screen_main()
-	elif ks == ":q":
-		EXIT = False
-		return
-	else:
-		decrypt_entry()
+    prompt = "\t*** INPUT ENTRY LABEL                         ***"
+    menu = "\t***              Locker - Decrypt             ***"
+    ks = gen_screen(prompt ,'LABEL: ', True).strip().lower()
+    if ks == ":q":
+        screen_main()
+    else:
+        lockops.decrypt_entry()
 
 def screen_import():
-	global IFILE
-	os.system('cls')
-	menu = "\t***              Locker - Import              ***"
-	prompt = "\t***               INPUT FILENAME              ***"
+    global IFILE
+    os.system('cls')
+    menu = "\t***              Locker - Import              ***"
+    prompt = "\t***               INPUT FILENAME              ***"
 
-	ks = gen_screen(prompt, "FILENAME: ", True, menu).strip().lower()
-	if ks == ":back":
-		screen_main()
-	elif ks == ":q":
-		EXIT = False
-		return
-	else:
-		try:
-			with open(ks, "r") as test:
-				pass
-			IFILE = ks
-			screen_import2()
-		except FileNotFoundError:
-			print()
-			print(ks, " IS NOT A VALID FILE")
-			time.sleep(1)
-			screen_import()
+    ks = gen_screen(prompt, "FILENAME: ", True, menu).strip().lower()
+    if ks == ":q":
+        screen_main()
+    else:
+        try:
+            with open(ks, "r") as test:
+                pass
+            IFILE = ks
+            screen_import2()
+        except FileNotFoundError:
+            print()
+            print(ks, " IS NOT A VALID FILE")
+            time.sleep(1)
+            screen_import()
 
 def screen_import2():
-	global IFORMAT
-	global MTB
-	os.system('cls')
-	print("\t*************************************************")
-	print("\t***              Locker - Import              ***")
-	print("\t***                                           ***")
-	print("\t***                                           ***")
-	print("\t*** Using labels 'LABEL' and 'PWD':           ***")
-	print("\t*** Give format of file lines                 ***")
-	print("\t***                                           ***")
-	print("\t*** ex:                                       ***")
-	print("\t*** `Facebook - password` --> `LABEL - PWD`   ***")
-	print("\t***                                           ***")
-	print("\t*** :back   - back to main menu               ***")
-	print("\t*************************************************")
-	ks = input('FORMAT: ')
-	if ks == ":back":
-		screen_import()
-	elif ks == ":q":
-		EXIT = False
-		return
-	else:
-		IFORMAT = ks
-		farray = lockops.import_file(IFILE, IFORMAT, MTB)
+    global IFORMAT
+    global MTB
+    os.system('cls')
+    print("\t*************************************************")
+    print("\t***              Locker - Import              ***")
+    print("\t***                                           ***")
+    print("\t***                                           ***")
+    print("\t*** Using labels 'LABEL' and 'PWD':           ***")
+    print("\t*** Give format of file lines                 ***")
+    print("\t***                                           ***")
+    print("\t*** ex:                                       ***")
+    print("\t*** `Facebook - password` --> `LABEL - PWD`   ***")
+    print("\t***                                           ***")
+    print("\t***           :q   - previous menu            ***")
+    print("\t*************************************************")
+    ks = input('FORMAT: ')
+    if ks == ":q":
+        screen_import()
+    else:
+        IFORMAT = ks
+        try:
+            MTB = lockops.import_file(IFILE, IFORMAT, MTB)
+        except ValueError:
+            print("FORMAT NOT FOUND - Please input exact format of file lines")
+            time.sleep(1)
+            screen_import2()
 
 def screen_list():
-	global MTB
-	os.system('cls')
-	for key, val in MTB.items():
-		print(key + ": " + str(val))
-		print()
-	input("Continue: ")
+    global MTB
+    os.system('cls')
+    print(type(MTB))
+    try:
+        for key, val in MTB.items():
+            print(key + ": " + str(val))
+            print()
+    except:
+        print(MTB)
+        time.sleep(2)
+    input("Continue: ")
 
 def screen_main():
-	global EXIT
-	global MTB
-	os.system('cls')
-	print("\t*************************************************")
-	print("\t***                  Locker                   ***")
-	print("\t***                                           ***")
-	print("\t***                                           ***")
-	print("\t***                                           ***")
-	print("\t*** list   - list entries                     ***")
-	print("\t*** import - import from text file            ***")
-	print("\t*** new    - create new entry                 ***")
-	print("\t*** delete - delete entry                     ***")
-	print("\t*** decrypt- decrypt entry                    ***")
-	print("\t***                                           ***")
-	print("\t*** :q     - close application                ***")
-	print("\t*************************************************")
-	ks = input("INPUT: ").strip().lower()
-	if ks == 'exit':
-		EXIT = False
-	elif ks == ":q":
-		EXIT = False
-	elif ks == "list":
-		screen_list()
-	elif ks == "import":
-		screen_import()
-	elif ks == "decrypt":
-		screen_decrypt()
-	return
+    global EXIT
+    global MTB
+    os.system('cls')
+    print("\t*************************************************")
+    print("\t***                  Locker                   ***")
+    print("\t***                                           ***")
+    print("\t***                                           ***")
+    print("\t***                                           ***")
+    print("\t*** list   - list entries                     ***")
+    print("\t*** import - import from text file            ***")
+    print("\t*** new    - create new entry                 ***")
+    print("\t*** delete - delete entry                     ***")
+    print("\t*** decrypt- decrypt entry                    ***")
+    print("\t***                                           ***")
+    print("\t*** :q     - close application                ***")
+    print("\t*************************************************")
+    ks = input("INPUT: ").strip().lower()
+    if ks == 'exit':
+        EXIT = False
+    elif ks == ":q":
+        EXIT = False
+    elif ks == "list":
+        screen_list()
+    elif ks == "import":
+        screen_import()
+    elif ks == "decrypt":
+        screen_decrypt()
+    return
 
 def cntn_main():
-	while EXIT:
-		screen_main()
-
-	os.system('cls')
-	return
+    while EXIT:
+        screen_main()
+    if not lockops.dir_search(None, True):  # Save file
+        lockops.write_secure_tfile(MTB)
+    os.system('cls')
+    return
 
 #*************************************** MAIN ********************************
 os.system('cls')
 apwd = gen_screen("\t***          INPUT LOCKER COMBINATION         ***")
 if apwd != ":q":
-	if not lockops.authenticate(apwd):
-		prmpt = input("Combination is unrecognized. Create new repository? (y/n)")
-		if prmpt == "y":
-			lockops.ics(apwd)
-			cntn_main()
-	else:
-		MTB = json.loads((lockops.rsc()).decode('utf-8'))
-		time.sleep(2)
-		cntn_main()
+    if not lockops.dir_search(apwd, True):
+        prmpt = input("Combination is unrecognized. Create new repository? (y/n)")
+        if prmpt == "y":
+            lockops.init_secure_tfile(apwd)
+            cntn_main()
+    else:
+        # MTB = json.loads((lockops.read_secure_tfile()).decode('utf-8'))
+        MTB = lockops.read_secure_tfile()
+        cntn_main()
 os.system('cls')
--- lockops.py
+++ lockops.py
@@ -1,94 +1,112 @@
 try:
-	from cryptography.fernet import Fernet
+    from cryptography.fernet import Fernet
 except:
-	raise Exception("The system is not properly initialized")
+    raise Exception("The system is not properly initialized")
 import json
 import os, os.path
 import time
 
 from tempfile import mkstemp
 
-def authenticate(initkey):
-	fd, tpath = mkstemp()
-	directory = os.path.dirname(tpath)
-	os.close(fd)
-	os.remove(tpath)
-	for fname in os.listdir(directory):
-		if fname.endswith(initkey):
-			return True
-	return False
+ukey = ""
+
+def dir_search(initkey, bool=False):
+    global ukey
+    if initkey == None:
+        initkey = ukey
+    fd, tpath = mkstemp()
+    directory = os.path.dirname(tpath)
+    os.close(fd)
+    os.remove(tpath)
+
+    for fname in os.listdir(directory):
+        if fname.endswith(initkey) and fname[:2] != "+~":
+            apath = os.path.join(directory, fname)
+            if ukey == "":
+                ukey = initkey
+
+            if bool:
+                return True
+            return apath
+    return False
+
 
 def decrypt_entry():
-	print("decrypt")
+    print("decrypt")
+
 
 def import_file(filepath, lineformat, sray):
-	key = Fernet.generate_key()
-	f = Fernet(key)
+    global ukey
+    key = Fernet.generate_key()
+    f = Fernet(key)
 
-	lidx = lineformat.index('LABEL')
-	hidx =lineformat.index('PWD')
-	lblevel = 0
-	if lidx > hidx:
-		lblevel = 1
-		lidx = hidx
-		hidx = lineformat.index('LABEL')
+    lidx = lineformat.index('LABEL')
+    hidx = lineformat.index('PWD')
+    lblevel = 0
+    if lidx > hidx:
+        lblevel = 1
+        lidx = hidx
+        hidx = lineformat.index('LABEL')
 
-	pre = lineformat[:lidx].strip('LABEL').strip('PWD')
-	mid = lineformat[lidx:hidx].strip('LABEL').strip('PWD')
-	suf = lineformat[hidx:].strip('LABEL').strip('PWD')
+    pre = lineformat[:lidx].strip('LABEL').strip('PWD')
+    mid = lineformat[lidx:hidx].strip('LABEL').strip('PWD')
+    suf = lineformat[hidx:].strip('LABEL').strip('PWD')
 
-	label = ""
-	passwd = ""
+    with open(filepath, "r") as ifile:
+        for line in ifile:
+            loval = line[line.index(pre):line.index(mid)]
+            hival = line[line.index(mid) + len(mid):line.index(suf)]
+            if suf == "":
+                hival = line[line.index(mid) + len(mid):]
 
-	with open(filepath, "r") as ifile:
-		for line in ifile:
-			loval = line[line.index(pre):line.index(mid)]
-			hival = line[line.index(mid)+len(mid):line.index(suf)]
-			if suf == "":
-				hival = line[line.index(mid)+len(mid):]
+            label = loval
+            passwd = hival
+            if lblevel == 1:
+                label = hival
+                passwd = loval
 
-			label = loval
-			passwd = hival
-			if lblevel == 1:
-				label = hival
-				passwd = loval
+            sray[label] = f.encrypt(bytes(passwd, "UTF-8")).decode("UTF-8")
 
-			sray[label] = f.encrypt(bytes(passwd, "UTF-8")).decode("UTF-8")
+    write_secure_tfile(sray)
+    return read_secure_tfile()
 
-	try:
-		os.mkdir("sc")
-	except:
-		pass
-
-	if not load_fp():
-		raise Exception("No existing repository file")
-	scw(sray)
- 
+# Deprecated check to JSON file, may be reinstated later
+"""
 def load_fp():  # Retrieve JSON file
-	with open("sc/dump.json","r") as dfile:
-		fp = json.load(dfile)
-		if len(fp) > 0:
-			return fp
-	return False
+    global repo_path
+    with open("sc/dump.json", "r") as dfile:
+        fp = json.load(dfile)
+        if len(fp) > 0:
+            return fp
+    return False
+"""
 
 # https://www.logilab.org/blogentry/17873
 # https://docs.python.org/3/library/tempfile.html#tempfile.mkstemp
-def ics(cusuffix):  # Create secure tmp file
-	fd, tpath = mkstemp(suffix = cusuffix)
-	os.close(fd)
-	with open("sc/dump.json","w") as dfile:
-		json.dump(tpath, dfile)
+def init_secure_tfile(cusuffix):  # Create secure tmp file
+    global ukey
+    ukey = cusuffix
 
-def rsc(blocksize=5000):  # Read secure tmp file
-	fp = load_fp()
-	rscf = os.open(fp, os.O_RDONLY)
-	obj = os.read(rscf, blocksize)
-	os.close(rscf)
-	return obj
 
-def scw(data):  # Write secure tmp file
-	obj = bytes(json.dumps(data),"utf-8")
-	fp = load_fp()
-	scwf = os.open(fp, os.O_WRONLY)
-	os.write(scwf, obj)
-	os.close(scwf)
+def read_secure_tfile(blocksize=5000):          # Read secure tmp file
+    global ukey
+    repo_path = dir_search(ukey)
+    rfile = os.open(repo_path, os.O_RDONLY)
+    obj = os.read(rfile, blocksize)             # Retrieve object
+    os.close(rfile)
+    os.remove(repo_path)                        # Destroy file
+    return json.loads(obj.decode("UTF-8"))
+
+
+def write_secure_tfile(data):  # Write secure tmp file
+    global ukey
+    obj = json.dumps(data).replace('\\"','"')
+    bobj = bytes(obj, "utf-8")
+    try:
+        repo_path = dir_search(ukey)
+        os.remove(repo_path)
+    except TypeError:
+        pass
+    fd, repo_path = mkstemp(suffix=ukey)        #Create file
+    os.write(fd, bobj)
+    os.close(fd)
